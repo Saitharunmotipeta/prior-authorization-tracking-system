0f5baeadabc7b1796bec7b8b966b13ba7f5f0a36
Harshitha Dandige <harsh@gmail.com>
2026-06-18 06:12:33 -0700

    removed unnecessary files

---
+++ backend/PriorAuthorization.Payer.API/DTOs/FacilityDto.cs
@@ -0,0 +1,11 @@
+﻿namespace PriorAuthorization.Payer.API.DTOs
+{
+    public class FacilityDto
+    {
+
+        public int FacilityId { get; set; }
+
+        public string FacilityName { get; set; }
+
+    }
+}
