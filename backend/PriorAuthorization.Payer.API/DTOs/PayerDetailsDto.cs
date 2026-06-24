02b5cf46d7d3f03fc37f2c19085937de3e412b0b
Charan Gannoju <charangannoju@gmail.com>
2026-06-24 03:56:23 -0700

    implemented payer, icd, cpt end points

---
+++ backend/PriorAuthorization.Payer.API/DTOs/PayerDetailsDto.cs
@@ -0,0 +1,9 @@
+﻿namespace PriorAuthorization.Payer.API.DTOs
+{
+    public class PayerDetailsDto
+    {
+        public int PayerId { get; set; }
+
+        public string PayerName { get; set; } = string.Empty;
+    }
+}
