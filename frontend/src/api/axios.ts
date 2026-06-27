d0eb7b445b9455ef615f87735b7f3b803f481a2d
Saitharunmotipeta <motipetasaitharun@gmail.com>
2026-06-27 13:05:30 -0700

    shifted back the build constraints

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,21 +1,24 @@
 import axios from "axios";
 
 export const specialistApiClient = axios.create({
-  baseURL: "http://localhost:5001",
+  // baseURL: "http://localhost:5001",
+  baseURL: "https://localhost:7225",
   headers: {
     "Content-Type": "application/json"
   }
 });
 
 export const payerApiClient = axios.create({
-  baseURL: "http://localhost:5003",
+  // baseURL: "http://localhost:5003",
+  baseURL: "https://localhost:7190",
   headers: {
     "Content-Type": "application/json"
   }
 });
 
 export const managerApiClient = axios.create({
-  baseURL: "http://localhost:5002",
+  // baseURL: "http://localhost:5002",
+  baseURL: "https://localhost:7273",
   headers: {
     "Content-Type": "application/json"
   }
